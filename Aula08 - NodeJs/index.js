3f0eeabb7e433941ca772c3c1c91a445725a0db8
Benjamin Wainer Kersz <bwkersz@gmail.com>
2026-04-28 12:05:20 -0300

    Aula08 - NodeJS

---
+++ Aula08 - NodeJs/index.js
@@ -0,0 +1,13 @@
+//import { createServer } from 'node:http';
+var http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain' );
+    res.end('Oi galerinha!!!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
