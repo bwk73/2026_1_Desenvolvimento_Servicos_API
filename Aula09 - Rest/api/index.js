fb28a82749879ad98f168b96cf7be09223d465c0
Benjamin Wainer Kersz <bwkersz@gmail.com>
2026-05-26 09:51:16 -0300

    Aula09 - Rest: implementando GETs

--- Aula09 - Rest/api/index.js
+++ Aula09 - Rest/api/index.js
@@ -24,6 +24,39 @@ api.get("/", (req, res, next) => {
 })
 
 
+api.get( "/product" , (req, res, next) => {
+    conn("produto")
+        .leftJoin("categoria" , "produto.codCategoria" , "=" , "categoria.id")
+        .select("produto.*" , "categoria.nome AS cat")
+        .then( dados => res.json( dados ) )
+        .catch( next )
+})
+
+api.get( "/product/:idProd" , (req, res, next) => {
+    const id = req.params.idProd
+    conn("produto")
+        .leftJoin("categoria" , "produto.codCategoria" , "=" , "categoria.id")
+        .select("produto.*" , "categoria.nome AS cat")
+        .where( "produto.id" , id )
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+})
+
+api.get( "/category" , (req, res, next) => {
+    conn("categoria")
+        .then( dados => res.json( dados ) )
+        .catch( next )
+})
+
+api.get( "/category/:idCat" , (req, res, next) => {
+    const id = req.params.idcat
+    conn("categoria")
+        .where( "categoria.id" , id )
+        .first()
+        .then( dados => res.json( dados ) )
+        .catch( next )
+})
 
 api.listen(PORT, () => {
     console.log(`Servidor rodando em: http://${HOSTNAME}:${PORT}`)
